4d32e55821251c16407dec7f01b271ee67b87881
Veranika Shynkarova <VShynkarova@gomel.iba.by>
2020-06-18 11:53:23 +0300

    task-3 move card func to card component & add eslint

--- src/App.js
+++ src/App.js
@@ -1,76 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Header from './Header';
-import Card from './Card';
+import Header from './header';
+import Card from './card';
 
 class App extends Component {
-    state = {
-        isChecked: false,
-        isEditMode: false,
-        title: 'Caption',
-        tempTitle: '',
-        info: 'additional text',
-        tempInfo: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Caption',
+            info: 'additional text',
+        };
     }
 
-    editHandler = () => {
+    saveHandler = (title, info) => {
         this.setState({
-            isEditMode: !this.state.isEditMode,
-            isChecked: false
+            title,
+            info,
         });
     };
-
-    undoHandler = () => {
-        this.setState({
-            isEditMode: !this.state.isEditMode,
-            title: this.state.tempTitle || this.state.title,
-            info: this.state.tempInfo || this.state.info,
-        });
-    }
-
-    saveHandler = () => {
-        this.setState({
-            isEditMode: !this.state.isEditMode,
-            tempTitle: '',
-            tempInfo: ''
-        });
-    }
-
-    changeStyleHandler = () => {
-        this.setState({isChecked: !this.state.isChecked})
-    };
-
-    changeTitleHandle = (event) => {
-        if (this.state.tempTitle === '') {
-            this.setState({tempTitle: this.state.title})
-        }
-        this.setState({title: event.target.value});
-    }
-
-    changeInfoHandle = (event) => {
-        if (this.state.tempInfo === '') {
-            this.setState({tempInfo: this.state.info})
-        }
-        this.setState({info: event.target.value});
-    }
 
     render() {
+        const { title, info } = this.state;
         return (
             <div className="App">
-                <Header title="Header"/>
-                <div style={{margin: '1rem'}}>
-                    <Card
-                        title={this.state.title}
-                        info={this.state.info}
-                        isChecked={this.state.isChecked}
-                        isEditMode={this.state.isEditMode}
-                        save={this.saveHandler}
-                        undo={this.undoHandler}
-                        edit={this.editHandler}
-                        check={this.changeStyleHandler}
-                        changeTitle={this.changeTitleHandle}
-                        changeInfo={this.changeInfoHandle}
-                    />
+                <Header title="Header" />
+                <div style={{ margin: '1rem' }}>
+                    <Card title={title} info={info} onSave={this.saveHandler} />
                 </div>
             </div>
         );
