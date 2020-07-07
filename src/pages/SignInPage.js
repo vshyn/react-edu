6380b4739577b5b4cc9cf56fc1b259bdbe3bb74a
Veranika Shynkarova <VShynkarova@gomel.iba.by>
2020-07-07 10:18:24 +0300

    task-12 validate

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,38 +1,96 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, Field } from 'react-final-form';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class SignInPage extends React.Component {
-    handleClick = () => {
+    onSubmit = () => {
         this.props.history.push('/');
     };
 
+    required = (value) => (value ? undefined : 'Required');
+
+    isEmail = (value) => (/\S+@\S+\.\S+/.test(value) ? undefined : 'Must be email');
+
+    minLength = (value) =>
+        value.length > 8 ? undefined : 'Must be 8 symbols length';
+
+    hasNumber = (value) =>
+        /\d/.test(value) ? undefined : 'Must have minimum one number';
+
+    hasLetter = (value) =>
+        /[a-zA-Z]/.test(value) ? undefined : 'Must have minimum one letter';
+
+    composeValidators = (...validators) => (value) =>
+        validators.reduce(
+            (error, validator) => error || validator(value),
+            undefined
+        );
+
+    checkDisable = (values) => values.hasValidationErrors;
+
     render() {
         return (
-            <Form>
-                <FormGroup>
-                    <Label for="username ">Username </Label>
-                    <Input
-                        type="username"
-                        name="username"
-                        id="username"
-                        placeholder="username"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="Password">Password</Label>
-                    <Input
-                        type="password"
-                        name="password"
-                        id="Password"
-                        placeholder="password"
-                    />
-                </FormGroup>
-                <Button color="primary" onClick={this.handleClick}>
-                    Sign in
-                </Button>
-            </Form>
+            <div>
+                <Form
+                    onSubmit={this.onSubmit}
+                    render={({ handleSubmit, form }) => (
+                        <form onSubmit={handleSubmit}>
+                            <Field
+                                name="username"
+                                validate={this.composeValidators(
+                                    this.required,
+                                    this.isEmail
+                                )}
+                            >
+                                {({ input, meta }) => (
+                                    <div>
+                                        <label>username</label>
+                                        <input
+                                            {...input}
+                                            type="text"
+                                            placeholder="username"
+                                        />
+                                        {meta.error && meta.touched && (
+                                            <span>{meta.error}</span>
+                                        )}
+                                    </div>
+                                )}
+                            </Field>
+                            <Field
+                                name="password"
+                                validate={this.composeValidators(
+                                    this.required,
+                                    this.minLength,
+                                    this.hasNumber,
+                                    this.hasLetter
+                                )}
+                            >
+                                {({ input, meta }) => (
+                                    <div>
+                                        <label>password</label>
+                                        <input
+                                            {...input}
+                                            type="text"
+                                            placeholder="password"
+                                        />
+                                        {meta.error && meta.touched && (
+                                            <span>{meta.error}</span>
+                                        )}
+                                    </div>
+                                )}
+                            </Field>
+                            <button
+                                type="submit"
+                                disabled={this.checkDisable(form.getState())}
+                                onSubmit={this.onSubmit}
+                            >
+                                Sign in
+                            </button>
+                        </form>
+                    )}
+                />
+            </div>
         );
     }
 }
